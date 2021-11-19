826809fb9ed2348eff9dd4360facde37b6e4007f
whatiskeptiname <susangmree@gmail.com>
2021-11-19 23:00:46 +0545

    decreased no. of movies in training set

--- tag_based_rs.py
+++ tag_based_rs.py
@@ -1,11 +1,27 @@
 import pickle
+import numpy as np
 import streamlit as st
 import requests
 
-
-movies = pickle.load(open("pickles/movie_list.p", "rb"))
+final_movies = pickle.load(open("pickles/movie_list.p", "rb"))
 similarity = pickle.load(open("pickles/similarity.p", "rb"))
+final_movies_titles = final_movies["title"]
 
-movie_list = [1, 2, 3]
 
-selected_movie = st.selectbox("Select a Movie", movie_list)
+def recommend(movie):
+    index = final_movies[final_movies["title"] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1]
+    )
+    for i in distances[1:6]:
+        return final_movies.iloc[i[0]].title
+
+
+# Streamlit Design
+
+selected_movie = st.selectbox("Select a file", final_movies_titles)
+
+recommend_movies = recommend(selected_movie)
+
+for i in recommend_movies:
+    st.text(i)
